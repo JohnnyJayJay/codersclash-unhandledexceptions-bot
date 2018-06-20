be0066bd69ad439983d7e4ee8d15a9ea2cc262e3
Hax <arzberger52@gmail.com>
2018-06-20 21:39:51 +0200

    adding ConfigSystem (not working)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 
+    }
+
+    private static void config() {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("config.yml"));
+            ArrayList<String> strings=new ArrayList<>();
+            for (String line = bufferedReader.readLine(); line != null;) {
+                strings.add(line);
+                line = null;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
