cfe4cb14b9698513aab877622f20af538092b8ec
JohnnyJayJay <39348311+JohnnyJayJay@users.noreply.github.com>
2018-06-21 17:02:50 +0200

    Add files via upload

--- src/main/java/de/unhandledexceptions/codersclash/bot/core/Bot.java
+++ src/main/java/de/unhandledexceptions/codersclash/bot/core/Bot.java
@@ -2,14 +2,14 @@ package de.unhandledexceptions.codersclash.bot.core;
 
 public class Bot {
 
-    //Attribute wie prefix, Token, alles, was vorher bestimmt werden kann über die config
+    // Attribute wie prefix, Token - alles, was vorher bestimmt werden kann über die config
 
     public Bot(/*Attributwerte*/) {
         // Attribute setzen
     }
 
     public void run() {
-        // Mit den gegebenen Attributen den Bot starten, JDA bauen
+        // Mit den gegebenen Attributen den Bot starten, ShardManager bauen
     }
 
 }
