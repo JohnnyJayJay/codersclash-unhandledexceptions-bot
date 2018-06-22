be8e4658501015656b4d21f1f91dd8c3eda604f5
JohnnyJayJay <39348311+JohnnyJayJay@users.noreply.github.com>
2018-06-22 21:25:10 +0200

    Add files via upload

---
+++ src/main/java/de/unhandledexceptions/codersclash/bot/commandapi/CommandSetException.java
@@ -0,0 +1,13 @@
+package de.unhandledexceptions.codersclash.bot.commands.commandapi;
+
+/**
+ * Exception that is thrown in case of any problems concerning the CommandSettings. <p>
+ * CommandSetExceptions are RuntimeExceptions.
+ * @author Johnny_JayJay
+ * @version 3.0
+ */
+public class CommandSetException extends RuntimeException {
+    public CommandSetException(String msg) {
+        super(msg);
+    }
+}
