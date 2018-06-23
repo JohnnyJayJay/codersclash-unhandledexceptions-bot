7b36ceb0c6d843c96a28b0f7b5443e21ef2d6c41
JohnnyJayJay <39348311+JohnnyJayJay@users.noreply.github.com>
2018-06-23 13:52:12 +0200

    Config als Attribut zur Klasse Bot hinzugefügt

--- src/main/java/de/unhandledexceptions/codersclash/bot/core/Bot.java
+++ src/main/java/de/unhandledexceptions/codersclash/bot/core/Bot.java
@@ -7,9 +7,11 @@ package de.unhandledexceptions.codersclash.bot.core;
 public class Bot {
 
     private final String TOKEN;
+    private final Config CONFIG;
 
-    public Bot(String token) {
+    public Bot(String token, Config config) {
         this.TOKEN = token;
+        this.CONFIG = config;
     }
 
     public void start() {
