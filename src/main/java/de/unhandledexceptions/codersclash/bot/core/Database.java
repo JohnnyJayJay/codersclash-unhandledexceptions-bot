4d7346dfb4ddb5ce89e14df0e3fb281e2cd67430
JohnnyJayJay <39348311+JohnnyJayJay@users.noreply.github.com>
2018-06-24 11:26:47 +0200

    Die DB Sachen nochmal rausgenommen (noch unsicher wie wir das machen)

--- src/main/java/de/unhandledexceptions/codersclash/bot/core/Database.java
+++ src/main/java/de/unhandledexceptions/codersclash/bot/core/Database.java
@@ -1,20 +1,7 @@
 package de.unhandledexceptions.codersclash.bot.core;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 public class Database {
 
-    private Connection connection;
 
-    public Database(String url, String username, String password) {
-        try {
-            this.connection = DriverManager.getConnection(url, username, password);
-        } catch (SQLException e) {
-            System.err.println("Database connection could not be established due to an SQLException.");
-            e.printStackTrace();
-        }
-    }
 
 }
