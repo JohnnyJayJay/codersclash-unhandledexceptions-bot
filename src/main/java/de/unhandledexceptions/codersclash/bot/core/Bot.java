26191c9c21f0a7c1c70a299a1d008bcb3da1c7b1
JohnnyJayJay <39348311+JohnnyJayJay@users.noreply.github.com>
2018-06-29 16:14:15 +0200

    default max shards auf 3 gesetzt, fehler beim starten transparenter gemacht

--- src/main/java/de/unhandledexceptions/codersclash/bot/core/Bot.java
+++ src/main/java/de/unhandledexceptions/codersclash/bot/core/Bot.java
@@ -8,6 +8,8 @@ import javax.security.auth.login.LoginException;
 
 public class Bot {
 
+    private int failCount;
+
     private final Config config;
     private final Database database;
 
@@ -16,6 +18,7 @@ public class Bot {
     private CommandSettings commandSettings;
 
     public Bot(Config config, Database database) {
+        this.failCount = 0;
         this.config = config;
         this.database = database;
         this.builder = new DefaultShardManagerBuilder();
@@ -30,8 +33,17 @@ public class Bot {
             this.shardManager = builder.build();
         } catch (LoginException e)
         {
-            System.err.println("[ERROR] Login failed, reloading... (Check your token in config.json)");
-            start();
+            if (++failCount < 3) {
+                System.err.println("[ERROR] Login failed, reloading... (Check your token in config.json)");
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e1) {}
+                start();
+            } else {
+                System.err.println("[ERROR] Login failed after 3 times. Exiting the program");
+                System.exit(1);
+            }
+
         }
 
         this.commandSettings = new CommandSettings(config.getPrefix(), this.shardManager, true);
