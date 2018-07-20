772fc569dd512a709e372beb6bda325281fd8f2c
Yann <yannci2002@gmail.com>
2018-07-20 17:11:45 +0200

    update

---
+++ src/main/java/de/unhandledexceptions/codersclash/bot/listeners/AutoChannelListener.java
@@ -0,0 +1,65 @@
+package de.unhandledexceptions.codersclash.bot.listeners;
+
+import de.unhandledexceptions.codersclash.bot.core.Database;
+import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.VoiceChannel;
+import net.dv8tion.jda.core.events.guild.voice.GuildVoiceJoinEvent;
+import net.dv8tion.jda.core.events.guild.voice.GuildVoiceLeaveEvent;
+import net.dv8tion.jda.core.events.guild.voice.GuildVoiceMoveEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author TheRealYann
+ * 20.07.2018
+ */
+
+public class AutoChannelListener extends ListenerAdapter {
+
+    private Database database;
+    private Set<VoiceChannel> channels = new HashSet<>();
+
+    public AutoChannelListener(Database database) {
+        this.database = database;
+    }
+
+    @Override
+    public void onGuildVoiceJoin(GuildVoiceJoinEvent event) {
+        var joined = event.getChannelJoined();
+        if (joined.getIdLong() == database.getAutoChannel(event.getGuild()))
+            createChannel(joined, event.getGuild(), event.getMember());
+    }
+
+    @Override
+    public void onGuildVoiceLeave(GuildVoiceLeaveEvent event) {
+        var left = event.getChannelLeft();
+        if (channels.contains(left) && left.getMembers().isEmpty())
+            left.delete().queue((v) -> channels.remove(left));
+    }
+
+    @Override
+    public void onGuildVoiceMove(GuildVoiceMoveEvent event) {
+        var left = event.getChannelLeft();
+        var joined = event.getChannelJoined();
+        if (channels.contains(left) && left.getMembers().isEmpty())
+            left.delete().queue((v) -> channels.remove(left));
+        if (joined.getIdLong() == database.getAutoChannel(event.getGuild()))
+            createChannel(joined, event.getGuild(), event.getMember());
+    }
+
+    private void createChannel(VoiceChannel channelJoined, Guild guild, Member member) {
+        guild.getController().createVoiceChannel("Channel by " + member.getEffectiveName())
+                .setUserlimit(channelJoined.getUserLimit())
+                .setParent(channelJoined.getParent())
+                .setTopic("Channel created by " + member.getUser() + "#"+ member.getUser().getDiscriminator() + " using the AutoChannel feature. User Limit [" + channelJoined.getUserLimit() + "]")
+                .queue((channel) -> {
+                    channels.add((VoiceChannel) channel);
+                    guild.getController().moveVoiceMember(member, (VoiceChannel) channel).queue();
+                    channel.createPermissionOverride(member).setAllow(Permission.ALL_CHANNEL_PERMISSIONS).queue();
+                });
+    }
+}
--- src/main/java/de/unhandledexceptions/codersclash/bot/listeners/ReadyListener.java
+++ src/main/java/de/unhandledexceptions/codersclash/bot/listeners/ReadyListener.java
@@ -1,9 +1,12 @@
 package de.unhandledexceptions.codersclash.bot.listeners;
 
 import de.unhandledexceptions.codersclash.bot.core.Config;
+import de.unhandledexceptions.codersclash.bot.core.Database;
 import de.unhandledexceptions.codersclash.bot.util.Logging;
+import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Icon;
 import net.dv8tion.jda.core.entities.SelfUser;
+import net.dv8tion.jda.core.entities.VoiceChannel;
 import net.dv8tion.jda.core.events.ReadyEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 import org.slf4j.Logger;
@@ -11,6 +14,8 @@ import org.slf4j.Logger;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author TheRealYann
@@ -18,12 +23,14 @@ import java.net.URL;
 
 public class ReadyListener extends ListenerAdapter {
 
+    private Database database;
     private Config config;
     private URL iconURL;
 
     private static Logger logger = Logging.getLogger();
 
     public ReadyListener(Config config) {
+        this.database = database;
         this.config = config;
         try {
             this.iconURL = new URL(config.getIconURL());
@@ -34,7 +41,8 @@ public class ReadyListener extends ListenerAdapter {
 
     @Override
     public void onReady(ReadyEvent event) {
-
+        List<Guild> guilds = event.getJDA().getGuilds().stream().collect(Collectors.toList());
+        //TODO List<VoiceChannel> voiceChannels = database.getAutoChannel();
         SelfUser selfUser = event.getJDA().getSelfUser();
         try {
             if (selfUser.getAvatarUrl() == null) {
@@ -48,5 +56,7 @@ public class ReadyListener extends ListenerAdapter {
         if (!selfUser.getName().equals(config.getBotName())) {
             selfUser.getManager().setName(config.getBotName()).queue();
         }
+
+
     }
 }
